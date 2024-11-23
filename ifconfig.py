922817d57fc655e6074867dd6f3e76c91ff9e4b4
yeapllg <52744942+yeapllg@users.noreply.github.com>
2024-11-23 13:44:45 +0800

    Add files via upload

---
+++ ifconfig.py
@@ -0,0 +1,12 @@
+
+'''
+1:stock_id请去自己的券商处获取,然后填入即可
+2:path填写miniqmt的路径
+3:email填写自己的邮箱,用于接收委托提醒
+4:max_money为每次委托最大金额,默认最少买入1手,可以根据自己情况自行设定.
+'''
+
+stock_id = "123456789"
+path = 'C:\\e海方舟-量化交易版\\userdata_mini'
+email = 'xxxxx@qq.com'
+max_money = 0
